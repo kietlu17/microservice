dca584bc60c032178e892c2edaedc2ed675c5b38
kietlu17 <kietnguyen17042004@gmail.com>
2025-10-19 20:59:51 +0700

    modifies .env

--- api-gateway/config.js
+++ api-gateway/config.js
@@ -4,5 +4,5 @@ module.exports = {
     auth_service : process.env.AUTH_SERVICE,
     order_service : process.env.ORDER_SERVICE,
     product_service : process.env.PRODUCT_SERVICE,
-    port : process.env.PORT
+    port : process.env.PORT_AUTH
 };
--- product/src/config.js
+++ product/src/config.js
@@ -5,6 +5,6 @@ module.exports = {
   rabbitMQURI: process.env.RABBITMQ_URI,
   rabbitMQQueueOrder:  process.env.RABBITMQ_QUEUE_ORDERS,
   rabbitMQQueueProduct:  process.env.RABBITMQ_QUEUE_PRODUCTS,
-  port: process.env.PORT,
+  port: process.env.PORT_PRODUCT,
   jwt_key: process.env.JWT_SECRET
 };
